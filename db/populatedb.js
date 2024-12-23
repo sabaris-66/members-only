f7e11ddc2d8f48f9d8bd7a712913d2768a618621
Sabaris <sabaris.elango@gmail.com>
2024-12-23 17:00:36 +0530

    Change model

--- db/populatedb.js
+++ db/populatedb.js
@@ -3,9 +3,9 @@ const { Client } = require("pg");
 
 const SQL = `
 create table if not exists members (
-    member_id integer primary key generated always as identity,
-    first_name varchar(255),
-    last_name varchar(255),
+    member_id integer generated always as identity,
+    user_id varchar(255) primary key,
+    full_name varchar(255),
     password varchar(255),
     status varchar(255)
 );
@@ -14,16 +14,16 @@ create table if not exists messages(
     message_id integer primary key generated always as identity,
     message varchar(255),
     date varchar(255),
-    member_id integer,
-    constraint fk_member foreign key (member_id)
-    references members(member_id)
+    user_id integer,
+    constraint fk_member foreign key (user_id)
+    references members(user_id)
 );
 
-insert into members (first_name, last_name, password, status)
+insert into members (user_id, full_name, password, status)
 values ('saba', 'saba', 'bigBull@9', 'admin');
 
-insert into messages (message, date,  member_id)
-values ('Wake Up','Good time :)', 1);
+insert into messages (message, date,  user_id)
+values ('Wake Up','Good time :)', 'saba');
 `;
 
 async function main() {
